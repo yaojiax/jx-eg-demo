b0097fe6b64a1bb8176fbe43b3b5201bc974c08c
Jiaxin Yao <jiaxinyao@google.com>
2024-08-27 22:49:57 +0800

    updated

--- demo.py
+++ demo.py
@@ -13,65 +13,58 @@ from vertexai.generative_models import (
 
 #Change the variables here as required!
 
-PROJECT_ID = "jiaxin-project"  # Your Google Cloud Project ID
-LOCATION = "us-central1"  # Your Google Cloud Project Region
+PROJECT_ID = os.environ.get("jiaxin-project")  # Your Google Cloud Project ID
+LOCATION = os.environ.get("us-central1")  # Your Google Cloud Project Region
 vertexai.init(project=PROJECT_ID, location=LOCATION)
-header_text = "Test"
-header_img_link = "https://storage.mtls.cloud.google.com/jx-demo-placeholder-media/header_image~2.png"
-reports_string_array = [
-            "RB1_General_Safety",
-            "RB10_Exceptional_Situations",
-            "RB11_Signalling",
+header_text = "Demo"
+header_img_link = "https://storage.mtls.cloud.google.com/jx-eg-demo/enggood.png"
+report_string_array = [
+            "Geography lesson - Topic 2.3 Sustainable Tourism Development",
 ]
-your_pdf_storage_prefix = "gs://smrtexcel/"
+your_pdf_storage_prefix = "gs://jx-eg-demo/"
 pdf_sample_question_array = [
-        "How can a staff member gain access to the track?",
-        "What happens if there is an air burst in the rear EMU?",
-        "The train in automatic mode is not automatically proceeding when the route signal is cleared. What should I do?"
+        "Cite where the arguments made in the audio file can be found in the slides. Give the exact file name and page of the slide.",
+
     ] # Takes in as many as you want
 
 pdf_prompt = f"""
-                Your are a very professional document summarization specialist and you pay key attention to the details
-                Look through the whole document carefully
-                Please answer the following questions, and highlight any assumptions made:
+                Your are a teacher's assistant looking to help make citations and references for classrom discussions to textbook materials.
+                Please answer the following questions:
                 """
 audio_geolocation_url = (
-        "https://storage.mtls.cloud.google.com/zysmrt-media/incident.mp3"
+        "https://storage.mtls.cloud.google.com/jx-eg-demo/Geography%20lesson%20-%20audio.MP3"
     )
 
-audio_file_uri = "gs://zysmrt-media/incident.mp3"
+audio_file_uri = "gs://jx-eg-demo/Geography lesson - audio.MP3"
 audio_type = "audio/mp3"
 audio_prompt = """
                     You are a transcriber specializing in transcribing Singaporean conversations.
                     Be as accurate as possible in the transcription, give me as many rows as possible.
-                    The given audio is an argument between 2 commuters on a train. 
-                    Please label the speaker as person A and person B, and present it in a table form
+                    The given audio is in a classroom setting, with a teacher and many students.
+                    Please label the teacher as Teacher, and the students as Student A Student B etc etc, and present it in a table form
                     with the fields speaker and text
-                    After analyzing the file please a detailed_summary of the argument
+                    After analyzing the file please a detailed_summary of the transcriptions
                 """
 audio_sample_output= """
-           | Speaker | Text |
-           |---|---|
-           | Person A | 为什么我不可以讲呢？ (Wèishénme wǒ bù kěyǐ jiǎng ne?) |
-           | Person B | What's your problem? |
-           | Person A | What's your problem? |
-           | Person B | I said to the old senior people, I said can you give your seat to the old people? |
-           | Person A | 啊，这里没有人要坐。(Ā, zhèli méiyǒu rén yào zuò.) |
-           | Person B | I said please can you? You just say please, just now. You cannot be rude to people. I never ask you, please, because you are youngster. |
-           | Person A | I am also woman. <noise> 你不可以乱乱骂。我刚才讲了我肚子痛，我没有听到，你还要讲。 你还是欧巴桑，要懂得-- |
-           | Person B | You see, but you have nothing like this. Why you want to lie? |
-           | Person A | 我没有骗你。(Wǒ méiyǒu piàn nǐ.) |
-           | Person B | You are Singaporean, you lose the face of Singapore, you know? You lose the, you lose the face of Singapore, you know? Lose the face. You lose the face, yes. You Singapore-- |
-            \n
-            Detailed Summary
-            This audio clip captures a heated argument between two commuters (Person A and Person B) on a Singaporean train.
+    | Speaker | Text |
+    |---|---|
+    | **Teacher** | So the inquiry question is: Mauritius, is tourism the way to go? So we have three groups supporting yes, tourism should be done, and we have three groups supporting no, it shouldn't be done. |
+    | **Student A** | We are well aware the tourism sector contribute to one quarter of our local economy. |
+    | **Student B** | You guys claim that you guys want to invest more money into tourism, correct? Why don't we invest the money into upgrading our sugarcane plantation so that they are more resistant to rising water temperatures which is caused by the increased carbon footprint which is produced by tourist activity? |
+    | **Student C** | Sugarcane is affected by natural disasters. We cannot control them. So what do you want the government to do? Did you read the background information properly? Because it's the natural disaster that is affecting sugarcane and not your carbon footprint. Thank you. |
+    | **Teacher** | Okay. No, no, no, no more questions. No, no. No back and forth. Okay. So pass down the reflection sheet. Okay, while we discuss for about five minutes, you complete the post-conference reflections. |    Detailed Summary:
+    
+    \n
+    This transcription captures a lively classroom debate in Singapore. The central question is whether tourism is the right path for Mauritius.
 
-            The argument stems from Person B asking Person A to give up their seat for an elderly passenger. Person B claims they politely asked for the seat ("I said please can you?"), while Person A argues they were not informed of the request ("啊，你没有告诉我！").
+    \n
+    Student A, representing the pro-tourism side, highlights tourism's significant contribution to the local economy.
+    Student B, opposing excessive reliance on tourism, challenges the pro-tourism group's proposal to invest more in the sector. They suggest investing in sugarcane plantations instead, aiming to enhance their resilience against climate change impacts, which they argue are exacerbated by tourism.
+    Student C, also seemingly against over-dependence on tourism, retorts that natural disasters, not carbon footprint from tourism, are the primary threat to sugarcane. They question the opposing group's grasp of the background information.
+    The Teacher, mediating the debate, calls for order, preventing further back-and-forth arguments. They then instruct the students to complete a reflection sheet while a separate discussion takes place.
+    The debate showcases critical thinking, with students presenting economic and environmental arguments. The use of Singaporean English is evident in the vocabulary and pronunciation.
+    """
 
-            The situation escalates quickly with both parties exchanging accusations. Person B criticizes Person A's behavior, suggesting they are being disrespectful and are giving Singaporeans a bad name ("You think you are Singaporean, you lose the face of Singapore, you know?"). Person A, feeling wrongly accused, defends themselves by stating they were not being impolite ("我没有不礼貌！").
-
-            The audio ends abruptly, leaving the resolution of the argument unknown. However, the conversation highlights the sensitivity surrounding respect for the elderly on public transport in Singapore and the potential for misunderstandings to escalate quickly.    
-            """
 # change prompts here as follow
 
 # Logic as below!
@@ -88,7 +81,6 @@ def load_models():
     multimodal_model_pro = GenerativeModel("gemini-1.5-pro-001")
     return multimodal_model_flash, multimodal_model_pro
 
-
 st.image(header_img_link)
 st.header(header_text, divider="rainbow")
 multimodal_model_flash, multimodal_model_pro = load_models()
@@ -100,11 +92,10 @@ tab1, tab2 = st.tabs(
 )
 
 with tab1:
-    st.subheader("Creating a QnA Chatbot to give replies based on rulebooks", divider='gray')
+    st.subheader("Citing information in lessons to the slides", divider='gray')
     st.markdown("""
-                Gemini is able to ingest large amounts of unstructured data such as PDFs and become a search engine
-                for questions related to the PDFs, such as the rulebook. It is also able to cite where exactly
-                the information is from, to allow for quick validation. \n
+                Gemini is able to ingest large amounts of unstructured data such as PDFs and audio 
+                to help with the citations of materials. \n
                 """)
 
     if "text_input_value" not in st.session_state:
@@ -126,15 +117,20 @@ with tab1:
     print(f"report_question: {question}")
 
     uc1_reports = st.multiselect(
-        "Select the reports you want to include. \n\n",
+        "Select the slides you want to include. \n\n",
         [
-            "RB1_General_Safety",
-            "RB10_Exceptional_Situations",
-            "RB11_Signalling"
+            "Geography lesson - Topic 2.3 Sustainable Tourism Development",
         ],
         key="uc1_reports",
     )
 
+    uc1_audio = st.multiselect(
+        "Select the audio you would like to reference. \n\n",
+        [
+            "Geography lesson - audio",
+        ],
+        key="uc1_audio",
+    )
 
     pdf_prompt = f"""
                 Analyze the document(s) carefully. 
@@ -147,7 +143,7 @@ with tab1:
     if generate_t2t_uc1 and pdf_prompt:
         print(f"report_question: {question}")
         with st.spinner("Analyzing using Gemini 1.5 Pro ..."):
-            first_tab1, first_tab2 = st.tabs(["Analysis", "Prompt"])
+            first_tab1, first_tab2, first_tab3 = st.tabs(["Analysis", "Prompt", "Sample"])
             with first_tab1:
                 #model = GenerativeModel(model_name="gemini-1.5-pro-001")
                 contents =[]
@@ -156,6 +152,11 @@ with tab1:
                     print(pdf_file_uri)
                     pdf_file = Part.from_uri(pdf_file_uri, mime_type="application/pdf")
                     contents.append(pdf_file)
+                for audio in uc1_audio:
+                    audio_uri = your_pdf_storage_prefix + audio + ".MP3"
+                    print(audio_uri)
+                    audio_file = Part.from_uri(audio_uri, mime_type=audio_type)
+                    contents.append(audio_file)
                 contents.append(pdf_prompt)
                 generation_config = {
                         "max_output_tokens": 8192,
@@ -175,6 +176,34 @@ with tab1:
                     st.write(response.text)
             with first_tab2:
                 st.text(pdf_prompt)
+            with first_tab3:
+                st.markdown(
+                    """
+Response:
+
+## Arguments for and Against Investing in Tourism in Mauritius
+
+### Argument For Investing in Tourism
+
+- **Tourism's Contribution to the Economy**: 
+  - The audio states, "The tourism sector contributes to one-quarter of our local economy." 
+  - This argument is found on slide 2, in the infographic titled **"SUSTAINABLE TOURISM"**.
+  - The infographic highlights:
+    - "Tourism is responsible for 235 million jobs in the world."
+    - Tourism is the "Main income source for many developing countries."
+
+### Argument Against Investing in Tourism
+
+- **Environmental Impact**:
+  - The opposing team suggests investing in upgrading sugarcane plantations to be more resistant to rising water temperatures caused by the increased carbon footprint of tourist activity.
+  - This argument is based on the information provided on slide 3, which states:
+    - "The growth of tourism and sugar plantations industries has [an] impact on the biodiversity of the country."
+    - Highlights the "ecological destruction" caused by tourism-related activities.
+    
+- **Impact of Natural Disasters**:
+  - The audio mentions the impact of natural disasters on sugarcane plantations. However, this point is refuted as not being directly related to the carbon footprint of tourism.
+                    """
+                )
 
 
 with tab2:
